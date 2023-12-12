66e07751f0f5ca6f319d1d037823128fe62d82df
sagar6777 <153580948+sagar6777@users.noreply.github.com>
2023-12-12 10:59:23 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int jaishreeram;
+	printf("enter any number \n");
+	scanf("%d",&jaishreeram);
+	if(jaishreeram%2==0)
+	{
+		printf("\n%d is even number",jaishreeram);
+	}
+	else
+	printf("\n%d is  odd number",jaishreeram);
+	return 0;
+	
+}
---
+++ 3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float a;
+	scanf("%f",&a);
+	printf("%f",a*2.54);
+	return 0;
+	
+}
